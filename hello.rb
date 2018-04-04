065df6a78a8124e3cb135d97beb5414626734e97
Bryan Huang <bry.huang@gmail.com>
2018-04-04 23:59:33 +0000

    new

---
+++ hello.rb
@@ -0,0 +1,10 @@
+puts "hello world!"
+puts "My name is Bryan!"
+
+gift = [ "puppy", "cat", "treat", "gift card", "letter" ]
+
+puts "All I got for Valentine's day was a #{gift.sample}."
+
+puts "my new branch"
+
+hello = "why hello there"
