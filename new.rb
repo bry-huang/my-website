0648a657e22901d3ed45890e76fdd698c442e2dd
Bryan Huang <bry.huang@gmail.com>
2018-04-05 00:58:26 +0000

    new

--- new.rb
+++ new.rb
@@ -2,3 +2,4 @@ breeds = ["corgis", "shiba inus", "jack russells", "huskies", "akita inus"]
 
 puts "Hi, my name is Bryan and I love #{breeds.sample}!"
 puts "Get me a puppy!"
+puts "I WANT A PUPPY!"
